93e93af5f417a29b8c293d29f18abf5c95fee644
supgaiard-pixel <sup.gaiard@gmail.com>
2025-11-09 22:37:29 +0400

    Fix: Retirer capture=environment pour permettre choix caméra/galerie sur mobile

--- components/WishlistEditor.tsx
+++ components/WishlistEditor.tsx
@@ -610,7 +610,6 @@ export function WishlistEditor({ initialFreeText = '', initialItems = [], groupI
               ref={fileInputRef}
               type="file"
               accept="image/*"
-              capture="environment"
               onChange={handleFileChange}
               className="hidden"
               id="image-upload"
